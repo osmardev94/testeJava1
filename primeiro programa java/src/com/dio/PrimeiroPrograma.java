11e0d4cd7ef968335cbc8b2f281319e2073d5d64
Osmar Domingos <osmar.dev94@gmail.com>
2023-03-18 22:52:25 -0300

    criacao de classes e calculadora

---
+++ primeiro programa java/src/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,26 @@
+package com.dio;
+
+import com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*int a = 2;
+		int b = 3;
+		System.out.println("Hello World " + (a+b));*/
+		
+		Gato gato = new Gato();
+		Livros livros = new Livros();
+		
+		System.out.println(gato);
+		System.out.println(livros);
+	}
+
+}
+
+
+class Livros {
+	private String nome;
+	private String npag;
+}
